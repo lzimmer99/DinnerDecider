f30e7085af9fca9dc017617c4d6684952738f51e
luiszimmermann <lzimmer1@stud.hs-heilbronn.de>
2020-11-10 17:19:21 +0100

    Added Comments

--- app/src/main/java/com/example/dinnerdecider/MainActivity.kt
+++ app/src/main/java/com/example/dinnerdecider/MainActivity.kt
@@ -9,13 +9,14 @@ class MainActivity : AppCompatActivity() {
 
     val foodList = arrayListOf("Indisches Curry", "Burger", "Maultaschen",
             "Pfannkuchen", "Spaghetti Carbonara")
-
+    // Liste aus welcher die Gerichte random herausgezogen werden
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         decideBtn.setOnClickListener {
+            //Aktion die ausgeführt werden soll wenn man auf unteren Button drückt
 
             val random = Random()
             val randomFood = random.nextInt(foodList.count()) //Array wird gezählt um random Nummer im Array Bereich zu erzeuget

0819059aad1b0434f249df23f0fe0d72c1534f73
luiszimmermann <lzimmer1@stud.hs-heilbronn.de>
2020-12-01 09:21:51 +0100

    Comments

--- app/src/main/java/com/example/dinnerdecider/MainActivity.kt
+++ app/src/main/java/com/example/dinnerdecider/MainActivity.kt
@@ -3,19 +3,17 @@ package com.example.dinnerdecider
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 import kotlinx.android.synthetic.main.activity_main.transfer
 
 class MainActivity : AppCompatActivity() {
 
-    val foodList = arrayListOf(
+    private val foodList = arrayListOf(
         "Indisches Curry", "Burger", "Maultaschen",
         "Khachapuri", "Minestrone", "Moussaka"
     )
     // Liste aus welcher die Gerichte random herausgezogen werden
-
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,11 +24,13 @@ class MainActivity : AppCompatActivity() {
 
 
         decideBtn.setOnClickListener {
-            //Aktion die ausgeführt werden soll wenn man auf unteren Button drückt
+            //Aktion die ausgeführt werden soll wenn man auf gelben Button drückt
 
             val random = Random()
-            val randomFood = random.nextInt(foodList.count()) //Array wird gezählt um random Nummer im Array Bereich zu erzeuget
+            val randomFood = random.nextInt(foodList.count())
+            //Array wird gezählt um random Nummer im Array Bereich zu erzeugen
             selectedFoodTxt.text = foodList[randomFood]
+            // zuweisen des inhalts von selectedFoodTxt.text
 
         }
 
--- app/src/main/java/com/example/dinnerdecider/YTActivity.kt
+++ app/src/main/java/com/example/dinnerdecider/YTActivity.kt
@@ -1,4 +1,3 @@
-@file:Suppress("NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
 
 package com.example.dinnerdecider
 
@@ -10,13 +9,14 @@ import com.google.android.youtube.player.YouTubePlayer
 import kotlinx.android.synthetic.main.activity_y_t.*
 
 
+@Suppress("NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
 class YTActivity : YouTubeBaseActivity() {
 
     companion object {
         const val YOUTUBE_API_KEY: String = "AIzaSyAsD5cg28hy42bnDZJmkgPoaaUshok8dp4"
         const val FOOD_Name = "food_passer"
     }
-
+    // Hier zum einen den API-Key und einen default String für Food_Name
 
     private lateinit var youtubePlayerInit: YouTubePlayer.OnInitializedListener
 
@@ -30,7 +30,7 @@ class YTActivity : YouTubeBaseActivity() {
 
     private fun initUI() {
 
-        //Array ähnlich Python Dictionary mit den zu den Gerichten zugehörigen Youtube-Video-Keys
+        // Array ähnlich Python Dictionary mit den zu den Gerichten zugehörigen Youtube-Video-Keys
         val ytarr: Map<String, String> = mapOf(
             "Indisches Curry" to "9DQ5QaAsCDs",
             "Burger" to "vCXg4tuMpe4",
@@ -38,20 +38,21 @@ class YTActivity : YouTubeBaseActivity() {
             "Khachapuri" to "UgofBvujNHA",
             "Minestrone" to "Usp-Rgyn2kc",
             "Moussaka" to "gIZKAuUh34I"
-
         )
 
         val vidId = ytarr[intent.getStringExtra(FOOD_Name)]
-
+        // über FOOD_Name wird die entsprechende Video-ID aus ytarr ausgelesen
 
 
         youtubePlayerInit = object : YouTubePlayer.OnInitializedListener {
             override fun onInitializationSuccess(
                 p0: YouTubePlayer.Provider?,
+                // Provides YoutubePlayer interface
                 youtubePlayer: YouTubePlayer?,
                 p2: Boolean
             ) {
                 youtubePlayer?.loadVideo(vidId)
+                // Hier wird das Video mit der entsprechenden  VideoID  geladen
             }
 
             override fun onInitializationFailure(
@@ -66,6 +67,7 @@ class YTActivity : YouTubeBaseActivity() {
             }
         }
 
+        // Klick auf den Button spielt das Video ab
         btnPlay.setOnClickListener{
             youtubePlayer.initialize(YOUTUBE_API_KEY, youtubePlayerInit)
         }
